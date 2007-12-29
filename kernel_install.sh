c2517754865e227de85014e2dbf86e8b1a631ba9
ludek <ludek@f2cf15b6-ae35-0410-ada4-ffa9fee6604c>
2007-12-29 20:21:11 +0000

    kernel install script
    git-svn-id: file://localhost/home/ludek/shm/gentoo-kernel-config@40 f2cf15b6-ae35-0410-ada4-ffa9fee6604c

---
+++ kernel_install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# System settings:
+ROOT=/dev/hda2
+ARCH=i386 #x86_64
+
+# extract verions
+KERNEL=`pwd | sed -e 's@.*/linux-@@'`
+KERBUILD=`sed -ne 's/CONFIG_LOCALVERSION="\(.*\)"/\1/p' .config`
+KERVER="$KERNEL$KERBUILD"
+
+echo -n install kernel "$KERVER" ' ?'
+read 
+
+cp -v ./arch/i386/boot/bzImage "/boot/kernel-$KERVER" || exit 1
+cp -v ./System.map "/boot/System.map-$KERVER" || exit 1
+cp -v ./.config "/boot/config-$KERVER" || exit 1
+
+
+echo "" >> /boot/grub/grub.conf
+echo "title=Linux $KERVER" >> /boot/grub/grub.conf
+echo kernel "/boot/kernel-$KERVER" "root=$ROOT" >> /boot/grub/grub.conf
