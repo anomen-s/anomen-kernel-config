72ce82974aaa9a078d8d7f263766d9136292505e
ludek <ludek@f2cf15b6-ae35-0410-ada4-ffa9fee6604c>
2008-12-11 17:55:08 +0000

    2.6.26-gentoo-r4-s22 init
    git-svn-id: file://localhost/home/ludek/shm/gentoo-kernel-config@90 f2cf15b6-ae35-0410-ada4-ffa9fee6604c

---
+++ 2.6.26-gentoo-r4-s22/kernel_install.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# kernell install script
+
+# $Id$ 
+
+# System settings:
+ROOT=`cat /etc/fstab | sed -ne 's@^[ \t]*\([^ \t][^ \t]*\)[ \t][ \t]*/[ \t].*@\1@p'`
+grep -q 'CONFIG_X86_32=y' .config && ARCH=i386
+grep -q 'CONFIG_X86_64=y' .config && ARCH=x86_64
+
+#ROOT=/dev/hda2
+
+echo chown portage...
+chown portage . -R
+
+echo make prepare
+su portage -c make prepare
+
+# extract verions
+#KERNEL=`make kernelversion`
+KERVER=`make kernelrelease`
+
+echo "Arch: $ARCH"
+echo "Root: $ROOT"
+#echo "Kern: $KERNEL"
+echo "KVer: $KERVER"
+
+echo -n Build '&' install kernel "$KERVER" ' ?'
+read 
+
+#echo Building kernel
+echo make clean...
+su portage -c make clean
+
+echo make all...
+su portage -c make all
+
+echo make modules_install...
+make modules_install || exit 10
+
+cp -v ./arch/$ARCH/boot/bzImage "/boot/kernel-$KERVER" || exit 1
+cp -v ./System.map "/boot/System.map-$KERVER" || exit 2
+cp -v ./.config "/boot/Config-$KERVER" || exit 3
+
+
+echo "" >> /boot/grub/grub.conf
+echo "title=Linux $KERVER" >> /boot/grub/grub.conf
+echo "kernel" "/boot/kernel-$KERVER" "root=$ROOT" "ro" >> /boot/grub/grub.conf
+
+echo "* INFO *"
+echo grub.conf was updated, but manual check is recomended.
+echo ""
+echo Finished.
