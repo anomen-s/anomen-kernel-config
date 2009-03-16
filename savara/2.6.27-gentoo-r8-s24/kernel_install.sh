d31f0a1841944c5c36fc1faa30693ea99b8efbc2
ludek <ludek@f2cf15b6-ae35-0410-ada4-ffa9fee6604c>
2009-03-16 11:35:49 +0000

    -s24
    git-svn-id: file://localhost/home/ludek/shm/gentoo-kernel-config@102 f2cf15b6-ae35-0410-ada4-ffa9fee6604c

---
+++ savara/2.6.27-gentoo-r8-s24/kernel_install.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# kernell install script
+
+# $Id$ 
+
+# System settings:
+ROOT=`cat /etc/fstab | sed -ne 's@^[ \t]*\([^ \t][^ \t]*\)[ \t][ \t]*/[ \t].*@\1@p'`
+
+echo chown portage...
+chown portage . -R
+
+echo make prepare
+make prepare
+
+# extract verions
+#KERNEL=`make kernelversion`
+KERVER=`make kernelrelease`
+
+echo "Root: $ROOT"
+#echo "Kern: $KERNEL"
+#echo "KVer: $KERVER"
+
+echo -n Build '&' install kernel "$KERVER" ' ?'
+read 
+
+#echo Building kernel
+echo make clean...
+make clean
+
+echo make all...
+su portage -c make all
+
+echo make modules_install...
+make modules_install || exit 10
+
+cp -v ./arch/x86/boot/bzImage "/boot/kernel-$KERVER" || exit 1
+cp -v ./System.map "/boot/System.map-$KERVER" || exit 2
+cp -v ./.config "/boot/Config-$KERVER" || exit 3
+
+
+echo "" >> /boot/grub/grub.conf
+echo "title=Linux $KERVER" >> /boot/grub/grub.conf
+echo "kernel" "/boot/kernel-$KERVER" "root=$ROOT" "ro" >> /boot/grub/grub.conf
+
+echo "* INFO *"
+echo grub.conf was updated, but manual check is recomended.
+echo ""
+echo Finished.
