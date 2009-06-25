200929c5527f5185db413648007f994f9ac208d6
ludek <ludek@f2cf15b6-ae35-0410-ada4-ffa9fee6604c>
2009-06-25 17:00:13 +0000

    -s28
    git-svn-id: file://localhost/home/ludek/shm/gentoo-kernel-config@125 f2cf15b6-ae35-0410-ada4-ffa9fee6604c

---
+++ savara/2.6.29-tuxonice-r3-s28/kernel_install.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# kernell install script
+
+# $Id$ 
+
+# System settings:
+ROOT=/dev/sda2
+
+echo make prepare
+make prepare || exit 5
+
+# extract verions
+KERVER=`make kernelrelease`
+
+echo "Root: $ROOT"
+
+echo -n Build '&' install kernel "$KERVER" ' ?'
+read 
+
+echo chown portage...
+chown portage . -R || exit 4
+
+echo make clean...
+su portage -c "make clean" || exit 6
+
+echo make prepare...
+su portage -c "make prepare" || exit 7
+
+echo make all...
+su portage -c "make all" || exit 8
+
+echo make modules_install...
+make modules_install || exit 10
+
+cp -v ./arch/x86/boot/bzImage "/boot/kernel-$KERVER" || exit 1
+cp -v ./System.map "/boot/System.map-$KERVER" || exit 2
+cp -v ./.config "/boot/Config-$KERVER" || exit 3
+
+
+echo "" >> /boot/grub/grub.conf
+echo "# #" >> /boot/grub/grub.conf
+echo "title=Linux $KERVER" >> /boot/grub/grub.conf
+echo "kernel" "/boot/kernel-$KERVER" "root=$ROOT" "ro" >> /boot/grub/grub.conf
+
+echo "* INFO *"
+echo grub.conf was updated, but manual check is recomended.
+echo ""
+echo Finished.
