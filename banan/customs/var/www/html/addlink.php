2d08af0b77e57531b5fedf82d358d0a1cfc7937b
anomen <ludek.h@gmail.com>
2016-04-07 13:10:38 +0200

    banan - customs - /var/wwww

--- banan/customs/var/www/html/addlink.php
+++ banan/customs/var/www/html/addlink.php
@@ -1,9 +1,11 @@
 <?php
 
-$urls="\n" . $_REQUEST['urls'];
+$urls=$_REQUEST['urls'] . "\n";
 
 $fn = '/var/www/html/list.txt';
+$fnh = '/var/www/html/history.txt';
 
 file_put_contents( $fn, $urls, FILE_APPEND);
+file_put_contents( $fnh, $urls, FILE_APPEND);
 
-header("Location: index.html");
+header("Location: /");
