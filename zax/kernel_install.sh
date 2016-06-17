c26f5684bab8d3f52535ec35df82c2278c384baf
anomen <ludek.h@gmail.com>
2016-06-17 08:38:36 +0200

    zax - kernel 4.4.6 (and 4.5.1) with HIGHMEM64 and DRM_TTM

--- zax/kernel_install.sh
+++ zax/kernel_install.sh
@@ -6,29 +6,40 @@
 
 # System settings:
 ROOT=/dev/sda1
+U=kernelbuilder
+
+if ! grep "$U:x:" /etc/passwd -q
+then
+ echo "User $U needed for building needed, create it using:"
+ echo "   useradd -m $U"
+ exit 4
+fi
+
+echo chown "$U"
+chown "$U" -R .
 
 echo make prepare
-make prepare || exit 5
+su $U -c  'make prepare' || exit 5
 
 # extract verions
-KERVER=`make kernelrelease`
+KERVER=`su $U -c  'make kernelrelease'`
 
 echo "Root: $ROOT"
 
-echo -n Build '&' install kernel "$KERVER" ' ?'
-read 
-
-#echo chown portage...
-#chown portage . -R || exit 4
+echo -n Building kernel "$KERVER"
+read
 
 echo make clean...
-make clean || exit 6
+su $U -c  'make clean' || exit 6
 
 echo make prepare...
-make prepare || exit 7
+su $U -c  'make prepare' || exit 7
 
 echo make all...
-make all || exit 8
+su $U -c  'make all' || exit 8
+
+echo -n Install kernel "$KERVER" ' ?'
+read 
 
 echo make modules_install...
 make modules_install || exit 10
@@ -39,11 +50,13 @@ make firmware_install || exit 11
 #echo mount /boot with rw
 #mount /boot -o remount,rw
 
+
 cp -v ./arch/x86/boot/bzImage "/boot/kernel-$KERVER" || exit 1
 cp -v ./System.map "/boot/System.map-$KERVER" || exit 2
 cp -v ./.config "/boot/Config-$KERVER" || exit 3
 cp -v ./Module.symvers "/boot/Module.symvers-$KERVER" || exit 3
 
+cp /boot/grub/grub.conf `mktemp -t grub.conf-XXXXXX`
 
 echo "" >> /boot/grub/grub.conf
 echo "# ? & ?" >> /boot/grub/grub.conf
