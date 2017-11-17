8d259cdcaf7b3c6978c8672811b99920461fec86
anomen <ludek.h@gmail.com>
2017-11-17 20:02:43 +0100

    zax

--- zax/home/.m2/update-pass.sh
+++ zax/home/.m2/update-pass.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 echo -n Password: 
-read -s P
-N=`expr substr  $P   10 1`
+read -s PP
+N=`expr substr  $PP   10 1`
 echo ""
-P=`exec mvn --encrypt-password   "$P" `
+P=`exec mvn --encrypt-password   "$PP" `
 echo $P $N
 sed -i -e "s!.*@REPLACEPASS@.*!$P  $N @REPLACEPASS@!"  settings.xml && echo settings.xml updated
 # 'NEW_PASSWORD'
 
+W=`whoami`
+T=`echo -n "$W:$PP" | base64`
+
+sed -i -e "s/.*Authorization.*/AddHeader \"Authorization\" \"Basic $T\"/" tinyproxy.conf  && echo tinyproxy.conf updated
