ce9643bb2cf69750adb7a63a0a7afde9c4cbf8b9
anomen <ludek.h@gmail.com>
2018-10-18 15:49:17 +0200

    zax - 4.14.65-gentoo-z23

--- zax/kernel_install.sh
+++ zax/kernel_install.sh
@@ -36,9 +36,6 @@ read
 echo make modules_install...
 sudo make modules_install || exit 10
 
-echo make firmware_install...
-sudo make firmware_install || exit 11
-
 #echo mount /boot with rw
 #mount /boot -o remount,rw
 
@@ -48,16 +45,8 @@ sudo cp -v ./System.map "/boot/System.map-$KERVER" || exit 2
 sudo cp -v ./.config "/boot/Config-$KERVER" || exit 3
 sudo cp -v ./Module.symvers "/boot/Module.symvers-$KERVER" || exit 3
 
-cp /boot/grub/grub.conf `mktemp -t grub.conf-XXXXXX`
-echo "" | sudo tee -a  /boot/grub/grub.conf
-echo "# ? & ?" | sudo tee -a  /boot/grub/grub.conf
-echo "title=Linux $KERVER" | sudo tee -a  /boot/grub/grub.conf
-echo "kernel" "/boot/kernel-$KERVER" "root=$ROOT" "ro" | sudo tee -a  /boot/grub/grub.conf
-echo "title=Linux $KERVER failsafe" | sudo tee -a  /boot/grub/grub.conf
-echo "kernel" "/boot/kernel-$KERVER" "root=$ROOT" "ro" "noresume" "nox" | sudo tee -a  /boot/grub/grub.conf
 
-echo "* INFO *"
-echo grub.conf was updated, but manual check is recomended.
+echo "GRUB2 not updated"
 echo ""
 echo Finished.
 
