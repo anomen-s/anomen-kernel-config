267fe7943a776425accb1396b9c7255283b1e01e
anomen <ludek.h@gmail.com>
2020-06-29 22:45:47 +0200

    banan

--- banan/customs/var/media/r-mode-off.sh
+++ banan/customs/var/media/r-mode-off.sh
@@ -3,7 +3,16 @@
 DLNA_DIR=/var/media
 MEDIA_DIR=/mnt/WD_ELEMENTS
 
+/usr/sbin/service minidlna stop
+
+sleep 3
+
 for A in filmy2 serialy2
 do
    ln -sfn "${MEDIA_DIR}/${A}" "${DLNA_DIR}/${A}"
 done
+
+sed -e 's/-restrict.conf/-full.conf/' -i '/etc/default/minidlna'
+
+/usr/sbin/service minidlna start
+
--- banan/customs/var/media/r-mode-on.sh
+++ banan/customs/var/media/r-mode-on.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+/usr/sbin/service minidlna stop
+
+sleep 3
 DLNA_DIR=/var/media
 
-for A in filmy2 serialy2
+for A in filmy2  serialy2
 do
    ln -sfn empty "${DLNA_DIR}/$A"
 done
+
+sed -e 's/-full.conf/-restrict.conf/' -i '/etc/default/minidlna'
+
+/usr/sbin/service minidlna start
