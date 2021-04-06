3d93a5e0a102a03644ab69ed3cf1d441492e0529
anomen <ludek.h@gmail.com>
2021-04-06 13:07:22 +0200

    malina4 - updates

--- malina4/customs/boot/overlays/build.sh
+++ malina4/customs/boot/overlays/build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-#mkdir -p decompiled
+mkdir -p decompiled
 
 for NAME in ds2482
 do
 echo "File: $NAME"
 dtc -@ -O dtb -o $NAME.dtbo -b 0 $NAME.dts
 
-#echo "decompile: $NAME"
-#dtc -O dts -o decompiled/$NAME.decompiled.dts -I dtb $NAME.dtb
+echo "decompile: $NAME"
+dtc -O dts -o decompiled/$NAME.decompiled.dts -I dtb $NAME.dtbo
 
 done
 
