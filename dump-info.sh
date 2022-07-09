03560236769dc5112ff8630632d8158510bafcf9
anomen <ludek.h@gmail.com>
2022-07-09 14:05:11 +0200

    dump-info.sh update

--- dump-info.sh
+++ dump-info.sh
@@ -31,7 +31,7 @@ run() {
  fi
 }
 
-for CMD in dmesg glxinfo hwinfo lshw lsmod vainfo uptime lscpu
+for CMD in dmesg glxinfo hwinfo lshw lsmod vainfo uptime lscpu vdpauinfo
 do
  run "$CMD" "" "$CMD"
 done
