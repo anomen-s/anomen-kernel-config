2cf6d79eec7bbe6466811132819895e98b98e698
anomen <ludek.h@gmail.com>
2022-12-08 23:14:23 +0100

    9XZ9SN2 - xkeyboard-config

--- 9XZ9SN2/setup/cz-prog.sh
+++ 9XZ9SN2/setup/cz-prog.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-wget https://raw.githubusercontent.com/anomen-s/anomen-overlay/master/x11-misc/xkeyboard-config/files/xkeyboard-config-cz_prog-2019.patch -O /tmp/xkeyboard-config-cz_prog-2019.patch
-
+#wget https://raw.githubusercontent.com/anomen-s/anomen-overlay/master/x11-misc/xkeyboard-config/files/xkeyboard-config-cz_prog-2019.patch -O /tmp/xkeyboard-config-cz_prog-2019.patch
+S=`pwd`
 cd /usr/share/X11/xkb
 
-sudo patch -p1 < /tmp/xkeyboard-config-cz_prog-2019.patch
+sudo patch -p1 < "$S"/xkeyboard-config-cz_prog-2019.patch
